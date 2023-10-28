5565bdb16ab7b124f58122f29fe5580e976c8a5e
York <york@pineal.ai>
2023-10-28 17:28:13 +0800

    fix bug

--- example/src/app/mod.rs
+++ example/src/app/mod.rs
@@ -2,6 +2,3 @@ pub mod event;
 pub mod market_maker_app;
 pub mod kline_publisher;
 pub mod price_consumer;
-pub mod price_feed_app;
-pub mod settings;
-pub mod price_feed;
--- example/src/main.rs
+++ example/src/main.rs
@@ -1,12 +1,9 @@
 mod app;
 
-use std::collections::HashMap;
 use app::kline_publisher::KlinePublisher;
 use app::market_maker_app::MarketMakerApp;
 use event_flow::app::AppEngine;
 use crate::app::price_consumer::PriceConsumerApp;
-use crate::app::price_feed_app::c;
-use crate::app::settings::get_settings;
 
 fn main() {
     let mut engine = AppEngine::new();
@@ -14,5 +11,4 @@ fn main() {
     engine.add_pub_app(Box::new(KlinePublisher::new()));
     engine.add_sub_app(Box::new(PriceConsumerApp::new()));
     engine.run();
-    // c();
 }
